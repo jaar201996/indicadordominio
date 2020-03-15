6d269218f1a17fbb31a61a5a5560f41a4d39af54
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-14 19:41:07 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -10,7 +10,7 @@
 			</div>
 			<div id="navbar" class="navbar-collapse collapse">
 				<ul class="nav navbar-nav ">
-					<li class="active"><a href="index.php">Lista de empleados</a></li>
+					<li class="active"><a href="index.php">Lista de dominios</a></li>
 					
 				</ul>
 			</div><!--/.nav-collapse -->
