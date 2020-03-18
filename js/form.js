942d6d86a79292c853e0ab44c335fd95485843d9
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-18 12:12:40 -0500

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,14 @@
+$('#formIndicador').submit(function (e){
+   e.preventDefault();
+   var numedident =  $.trim($("numedident").val());
+   var numedcident =  $.trim($("numedcident").val());
+   var edccatalog =  $.trim($("edccatalog").val());
+   var ednccatalog =  $.trim($("ednccatalog").val());
+   var rndefinidas =  $.trim($("rndefinidas").val());
+   var rnimplactejec =  $.trim($("rnimplactejec").val());
+   var rndesact =  $.trim($("rndesact").val());
+   var edtrazacatalog =  $.trim($("edtrazacatalog").val());
+   var edtrazafueracatalog =  $.trim($("edtrazafueracatalog").val());
+
+  alert(numedident);
+});
