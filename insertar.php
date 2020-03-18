189b46a8025d18b87f92f2948c54b361146e5328
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-18 18:06:04 -0500

    Update insertar.php

--- insertar.php
+++ insertar.php
@@ -154,7 +154,6 @@ Email	 	 : info@obedalvarado.pw
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="jquery/jquery-3.2.1.min.js"></script>
-	<script src="js/bootstrap-datepicker.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 	<script type="text/javascript">
 		function validaNumericos(event) {
