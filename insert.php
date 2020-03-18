344d1705cb5bbb86fbbea8b91fa3b254faadeb81
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-18 18:03:40 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,12 +1,18 @@
 <?php 
-	$conexion=mysqli_connect('localhost','root','','pruebas');
-
-	$nombre=$_POST['nombre'];
-	$apellido=$_POST['apellido'];
-	$usuario=$_POST['usuario'];
-	$password=sha1($_POST['password']);
-
-	$sql="INSERT into usuarios (nombre,apellido,usuario,password)
-			values ('$nombre','$apellido','$usuario','$password')";
-	echo mysqli_query($conexion,$sql);
- ?>
+	 include("conexion.php");
+         date_default_timezone_set('America/Lima');
+         $date = date("Y-m-d H:i:s");
+          $insert = pg_query($dbconn, "INSERT INTO indicador(coddominio, numedident, numedcident, edccatalog, ednccatalog, rndefinidas, rnimplactejec, rndesact,edtrazacatalog,fecactual,edtrazafueracatalog)
+															VALUES('$_GET[nik]',
+															       '$_POST[numedident]',
+															       '$_POST[numedcident]', 
+															       '$_POST[edccatalog]',
+															        '$_POST[ednccatalog]', 
+															        '$_POST[rndefinidas]', 
+															        '$_POST[rnimplactejec]', 
+															        '$_POST[rndesact]',
+															        '$_POST[edtrazacatalog]',
+															        '$date',
+															        '$_POST[edtrazafueracatalog]')") or die(pg_last_error($dbconn));
+         }
+?>
